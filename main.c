d683bf4c1d35a7ac89d47652fe36306fb1467d9c
manutije <48832967+manutije@users.noreply.github.com>
2020-09-14 09:56:11 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 struct proceso{
 
   int pid;
-  int rafaga;
+  int rafagas_CPU[5];
+  int rafagas_ES[5];
   int tiempo_llegada;
 };
 
@@ -23,7 +24,12 @@ int main(void){
   for(int i=0;i<20;i++){
     printf("\n PROCESO # %d\n",i+1);
     printf("PID: %d\n",proc[i].pid);
-    printf("RAFAGA: %d\n",proc[i].rafaga);
+    for(int j=0;j<5;j++){
+      printf("\nRAFAGAS %d\n",j+1);
+      printf("RAFAGA CPU: %d\n",proc[i].rafagas_CPU[j]);
+      printf("RAFAGA E/S: %d\n",proc[i].rafagas_ES[j]);
+    }
+    printf("\n");
     printf("LLEGADA: %d\n",proc[i].tiempo_llegada);
     printf("\n");
   }
@@ -35,11 +41,22 @@ int main(void){
 
 
 void generador_procesos(){
+  int temp=0;
   srand(time(NULL));
 
   for(int i =0;i<20;i++){
     proc[i].pid = i;
-    proc[i].rafaga = rand()%20;
+    for(int j =0;j<5;j++){
+      while(temp<10){
+        temp =rand()%20;
+      }
+      proc[i].rafagas_ES[j] = temp;
+      temp =0;
+      while(temp<1){
+        temp =rand()%5;
+      }
+      proc[i].rafagas_CPU[j] = temp;
+    }
     if(i == 0){
       proc[0].tiempo_llegada =0;
     }
