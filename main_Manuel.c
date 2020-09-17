53530ba5662a3a19d25c6503cebbdde58754a64c
manutije <48832967+manutije@users.noreply.github.com>
2020-09-17 07:41:24 -0500

    Add files via upload
    Se agrego un almacenamiento de los procesos en un archivo txt y ya funciona bien la cola de CPU

--- main_Manuel.c
+++ main_Manuel.c
@@ -15,6 +15,8 @@ typedef struct proceso procesos;
 
 struct cola_cpu {
   procesos process;
+  int cont_rafagas;
+  int tiempo_salida;
   struct cola_cpu *siguiente;
 };
 
@@ -23,7 +25,10 @@ typedef struct cola_cpu Nodo;
 Nodo *final;
 Nodo *inicio;
 
+
 procesos proc[20];
+int contador_global =0;
+int cpu_vacio =0;
 void generador_procesos( int ale);
 void imprimir_procesos(int ale);
 void ordenamiento();
@@ -32,6 +37,9 @@ int isEmpty();
 void enqueue(procesos nuevo_proceso);
 void clearBuffer();
 int menu();
+void nodo_en_cpu();
+void agregar_cpu();
+void dequeue();
 
 int main(void){
   int aleatorio =0;
@@ -44,6 +52,7 @@ int main(void){
   for(int i=0;i<20;i++){
     enqueue (proc[i]);
   }
+  agregar_cpu ();
   return menu ();
 }
 
@@ -78,20 +87,32 @@ void generador_procesos(int ale){
 
 
 void imprimir_procesos(int ale){
+  FILE *ofp;
+  ofp = fopen("procesos.txt","w");
+
   for(int i=0;i<20;i++){
     printf("\n PROCESO # %d\n",i+1);
+    fprintf(ofp,"\n PROCESO # %d\n",i+1);
     printf("PID: %d\n",proc[i].pid);
+    fprintf(ofp,"PID: %d\n",proc[i].pid);
     for(int j=0;j<ale+1;j++){
       printf("\nRAFAGAS %d\n",j+1);
+      fprintf(ofp,"\nRAFAGAS %d\n",j+1);
       printf("RAFAGA CPU: %d\n",proc[i].rafagas_CPU[j]);
+      fprintf(ofp,"RAFAGA CPU: %d\n",proc[i].rafagas_CPU[j]);
       if(j<ale){
         printf("RAFAGA E/S: %d\n",proc[i].rafagas_ES[j]);
+        fprintf(ofp,"RAFAGA E/S: %d\n",proc[i].rafagas_ES[j]);
       }
     }
     printf("\n");
+    fprintf(ofp,"\n");
     printf("LLEGADA: %d\n",proc[i].tiempo_llegada);
+    fprintf(ofp,"LLEGADA: %d\n",proc[i].tiempo_llegada);
     printf("\n");
+    fprintf(ofp,"\n");
   }
+  fclose(ofp);
 }
 
 int generar_aleatorio()
@@ -139,6 +160,8 @@ void enqueue(procesos nuevo_proceso) {
 
 
   nodoNuevo->process = nuevo_proceso;
+  nodoNuevo->cont_rafagas = 0;
+  nodoNuevo->tiempo_salida=0;
 
   printf("\nAgregando a la cola de cpu el proceso %d\n",nodoNuevo->process.pid);
   if (isEmpty()) { // si la cola esta vacia
@@ -165,6 +188,8 @@ int menu() {
 
   do {
     printf("\n~~ MENU: Simulador ~~");
+    printf("\n\nTiempo de la simulacion: %d",contador_global);
+    nodo_en_cpu ();
     printf("\nHaga una seleccion:");
     printf("\n 1. Limpiar pantalla");
     printf("\n 2. Avanzar");
@@ -187,6 +212,8 @@ int menu() {
       default:
         break;
     }
+    agregar_cpu ();
+    contador_global++;
   } while(c != 'q' && c != EOF);
 
   return 1;
@@ -195,4 +222,52 @@ int menu() {
 void clearBuffer() {
   while(getchar() != '\n')
     ;
+}
+
+void nodo_en_cpu(){
+  printf("\n\n Nodo que se encuentra actualmente en el CPU");
+  printf("\nPID: %d",inicio->process.pid);
+  printf("\nTiempo de llegada: %d",inicio->process.tiempo_llegada);
+  printf("\nTiempo de salida: %d",inicio->tiempo_salida);
+}
+void agregar_cpu(){
+  if(cpu_vacio == 0){
+    inicio->tiempo_salida =contador_global+inicio->process.rafagas_CPU[inicio->cont_rafagas];
+    cpu_vacio =1;
+  }
+  if(cpu_vacio == 1){
+    if(inicio->tiempo_salida == contador_global){
+      printf("\nYa acabo el proceso con PID: %d",inicio->process.pid);
+      dequeue ();
+      cpu_vacio =0;
+      agregar_cpu ();
+    }
+
+  }
+}
+
+void dequeue() {
+  Nodo *actual,
+       *temporal;
+
+
+  clearBuffer();
+
+  if (isEmpty()) {
+    printf("Y metiamos algo en la cola...");
+  } else {
+    if (final == inicio) { // si solo hay 1 nodo
+      free(final); // liberamos la memoria que uso el nodo
+      free(inicio); // liberamos la memoria que uso el nodo
+      final = inicio = 0; // dejamos los punteros en null
+    } else { // si hay mas de 1 nodo
+      actual = final;
+      while (actual != inicio) { // recorra la cola y quede en el penultimo
+        temporal = actual; // temporal guarda la direccion del penultimo
+        actual = temporal->siguiente;
+      }
+      free(inicio); // liberamos la memoria usada por el ultimo nodo
+      inicio = temporal; // el penultimo es ahora el ultimo
+    }
+  }
 }
